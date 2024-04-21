d9cdc4c488dc01427f9b3b0d04b18766fb44b1ba
yikiialvinlincoln <yikiialvinlincoln@gmail.com>
2024-04-21 13:48:55 +0300

    theory

---
+++ myproject.py
@@ -0,0 +1,15 @@
+#Kesho project
+#jumpers from around kibuli paid daily (3k)
+'''they are registered once at school;
+Names, location, date of birth, gender, next of kin, NIN, recommender's name, religion which is optional, level of education, jumper name, contacts'''
+
+#j_names, j_location, j_dob, j_gender, j_next of kin, j_level of education, j_sitter number, j_contact
+
+#Babes
+"""name of the baby, gender, age, location, name of the person that brought the baby, arrival, parent's name, fee in Ugx, period of stay, baby number"""
+
+#b_names, b_gender, b_age, b_location, b_sponsor, b_timeIn, b_timeOut, b_payment, b_periodOfStay 
+
+#django emphasizes simple approach over complex 
+#we are going to use one django project and one django app
+#venv is to separate projects of different versions 
